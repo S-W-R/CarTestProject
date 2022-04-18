5725b903a9ba96d5a2a95b69ebd6e1910fd072d6
S-W-R <sn_swr1@mail.ru>
2022-04-18 13:00:57 +0500

    Initial commit

---
+++ CarApi/Infrastructure/Models/CarDbEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace CarApi.Infrastructure.Models;
+
+public class CarDbEntity
+{
+    public string Number { get; set; }
+    public int RegionCode { get; set; }
+    public string CarType { get; set; }
+    public DateTime ManufactureDate { get; set; }
+}
---
+++ CarApi/Mappings/CarMapperProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using CarApi.Domain.Models;
+using CarApi.DTO;
+using CarApi.Infrastructure.Models;
+
+namespace CarApi.Mappings;
+
+public class CarMapperProfile : Profile
+{
+    public CarMapperProfile()
+    {
+        CreateMap<CarDbEntity, Car>()
+            .ForMember(x => x.CarNumber,
+                e => e.MapFrom(c => new CarNumber(c.Number, c.RegionCode)))
+            .ReverseMap()
+            .ForMember(x => x.Number, e => e.MapFrom(c => c.CarNumber.Number))
+            .ForMember(x => x.RegionCode, e => e.MapFrom(c => c.CarNumber.Code));
+        CreateMap<CarDTO, Car>()
+            .ForMember(x => x.CarNumber,
+                e => e.MapFrom(c => new CarNumber(c.Number, c.RegionCode)))
+            .ReverseMap()
+            .ForMember(x => x.Number, e => e.MapFrom(c => c.CarNumber.Number))
+            .ForMember(x => x.RegionCode, e => e.MapFrom(c => c.CarNumber.Code));
+    }
+}
